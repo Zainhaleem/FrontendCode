86ed4a0e72e0b077b1be07b59561f542207f6fc6
Wasif Hussain <140958615+WasifZaidi@users.noreply.github.com>
2025-05-25 05:01:27 -0700

    Update page.jsx

--- src/app/(allpages)/Appoinment/page.jsx
+++ src/app/(allpages)/Appoinment/page.jsx
@@ -45,7 +45,7 @@ const page = () => {
     const notifyError = () => toast("❌ There was a problem. Please contact us at +92 334 3966787.");
     const notify = () => toast("✅ Applied for Appoinment Successfully");
     emailjs
-      .sendForm('service_vef16rl', 'template_fw9eqww', form.current, {
+      .sendForm('service_vef16rl', 'template_yce5yyn', form.current, {
         publicKey: 'DP1ld3tUG8jz2_fr0',
       })
       .then(
